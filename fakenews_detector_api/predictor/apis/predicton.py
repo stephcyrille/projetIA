03fb26d92f34de64bfa13fc1cb9e49622f8f4c56
Aminazdieng <aminazeinab9@hotmail.fr>
2024-04-26 16:32:24 +0200

    création de séréalizer et mise à jour des views, création des méthodes get et post, modification des urls

--- fakenews_detector_api/predictor/apis/predicton.py
+++ fakenews_detector_api/predictor/apis/predicton.py
@@ -3,9 +3,20 @@ import pickle
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from predictor.models import PostRequest
+from predictor.serializer.PostRequestSerializer import PostRequestSerializer
 
 
-class PostPredictionRequest(APIView):
+class PostPredictionRequestGet(APIView):
+
+    def get(self, request):
+        queryset = PostRequest.objects.all()
+        serializer = PostRequestSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PostPredictionRequestPost(APIView):
+    
     def post(self, request):
         res = {
             "statusCode": '00201',
